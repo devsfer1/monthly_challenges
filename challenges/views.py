8525db45566413fc46062ee57e6ecd0b65c8bafc
Fernando Chaves <fernando.chaves16@hotmail.com>
2023-02-20 22:31:44 -0300

    chore: progress

--- challenges/views.py
+++ challenges/views.py
@@ -3,6 +3,21 @@ from django.http import HttpResponse, HttpResponseNotFound
 
 # Create your views here.
 
+monthly_challenges = {
+  "january": "Eat no meat for the entire month",
+  "february": "Walk for at least 20 minutes every day",
+  "march": "Learn Django for at least 20 minutes every day",
+  "april": "Eat no meat for the entire month",
+  "may": "Meditate for at least 10 minutes every day",
+  "june": "Read a book for at least 30 minutes every day",
+  "july": "Take a photo every day and create a photo journal at the end of the month",
+  "august": "Write a journal entry every day",
+  "september": "Learn a new word every day and use it in conversation",
+  "october": "Do a random act of kindness every day",
+  "november": "Take a break from social media for the entire month",
+  "december": "Do something creative every day, like drawing, painting or writing."
+}
+
 def index(request):
   return HttpResponse('Januaryy')
 
@@ -14,13 +29,11 @@ def monthly_challenge_by_number(request, month):
   return HttpResponse(month)
 
 def monthly_challenge(request, month):
-  challenge_text = None
-  if month == 'january':
-    challenge_text = 'Eat no meat for the entire month'
-  elif month == 'february':
-    challenge_text = 'Walk for at least 20 minutes every day'
-  elif month == 'march':
-    challenge_text = 'Learn Django for at least 20 minutes every day'
-  else:
-    return HttpResponseNotFound('invalid path')
-  return HttpResponse(challenge_text)
+  # print(request, month)
+  try:
+   challenge_text = monthly_challenges[month]
+   return HttpResponse(challenge_text)
+  except:
+    return HttpResponseNotFound('Month not supported')
+
+  # return HttpResponse(challenge_text)
